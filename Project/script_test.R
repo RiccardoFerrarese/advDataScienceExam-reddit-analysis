ef68f3b824a7eb37ebb753f9e6d03147abe61f99
RiccardoFerrarese <ferrarese.riccardo@gmail.com>
2021-04-23 14:28:52 +0200

    change branch -- update file

--- Project/script_test.R
+++ Project/script_test.R
@@ -1,30 +1,23 @@
-sentiment_bing <- function(x, y){
-   if({{x}} == 'positive' & {{y}} == 'positive'){
-      return('positive')
-   } else if( {{x}} == 'negative' & {{y}} == 'negative' ){
-      return('negative')
-   } else {
-      return('neutral')
-   }
-}
-
-sentiment_df <- doge_bigrams_counts %>% 
-   inner_join(get_sentiments("afinn"), by=c('word1'= 'word')) %>%
-   inner_join(get_sentiments("bing"), by=c('word1'= 'word')) %>%
-   inner_join(get_sentiments("afinn"), by=c('word2'= 'word')) %>%
-   inner_join(get_sentiments("bing"), by=c('word2'= 'word')) %>%
-   mutate( affin = (value.x + value.y) / 2) %>%
-   mutate( bing = case_when(sentiment.x == 'positive' & sentiment.y == 'positive' ~ 'positive', 
-                            sentiment.x == 'negative' & sentiment.y == 'negative' ~ 'negative',
-                            TRUE ~ 'neutral')) %>%
-   unite(words, c("word1", "word2"), sep = " ") 
-
-View(sentiment_df)
-## with affin make mean between value.x & value.y
-## with bing 
-   ## positive - positive --> positive
-   ## positive - negative --> neutral
-   ## negative - positive --> neutral
-   ## negative - negative --> negative
+data_clean <- doge_com_raw_data %>%
+   mutate(date = as.Date(as_datetime(created_utc))) %>%
+   mutate(id = row_number()) %>%
+   filter(author != '[deleted]' & author != 'AutoModerator') %>% 
+   distinct( author, date, body, parent_id, link_id, .keep_all= TRUE) %>%
+   select(id, date, author, body, parent_id, link_id) 
 
 
+
+# quanteda corpus function 
+corpus <- corpus( as.character(data_clean$body), 
+                  docnames = data_clean$id, 
+                  docvars = data.frame(author = data_clean$author,data =  data_clean$date, post = data_clean$link_id))
+
+summary(corpus)
+
+# install gsl [sudo apt-get install gsl-bin libgsl0-dev]
+library(topicmodels)
+LDA_fit_20 <- convert(doge_tidy_post$dfm, to = "topicmodels") %>% 
+   LDA(k = 20)
+
+# get top five terms per topic
+get_terms(LDA_fit_20, 5)
--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 Prof. Massimo Franceschet
 write by Dott. Riccardo Ferrarese
+
+
+
+Instruction for project's set up in the first section of the final report.
+
+Content of directory: 
+-  Project: dir with all the file and environment of the project 
+  - renv: dir with R env.  
+  - Report: dir with .md and .pdf final report
+  - Data: dir with all the data 
+  - bibliography.bib: file for prettifying the management of citation
+  
+- PythonScript: dir with the script for scrapping Reddit's data 
+- 
